f98a7a1ca15bf4dccfcf7ddf297bfe95a2bc5f49
Rachel Fish <itsrachelfish@gmail.com>
2014-08-27 23:44:59 -0600

    Bash script to load server passwords from a config file

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Load password from config.sh
+source config.sh
+
+config="$1.conf"
+temp="/tmp/teeworlds.$RANDOM.conf"
+
+# Copy existing config into temp file
+cat $config > $temp
+
+# Append password
+echo "sv_rcon_password $password" >> $temp
+
+teeworlds-server -f $temp
