a091c22ff358a7e352836eddccd305f1f1ae3382
Rachel Fish <itsrachelfish@gmail.com>
2017-03-19 16:08:40 -0600

    Added config example file

---
+++ config.example.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Copy this file and rename it to "config.sh" before starting a teeworlds server
+
+adminpass="admin changeme"
+modpass="moderator changeme"
+consolepass="console changeme"
