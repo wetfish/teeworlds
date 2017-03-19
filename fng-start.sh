573374acc0ff155857f7f9f1d9264998e72e2097
Rachel Fish <itsrachelfish@gmail.com>
2017-03-19 15:59:16 -0600

    Created fng start script, updated fng config with wetfish motd

---
+++ fng-start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Load password from config.sh
+source config.sh
+
+config="config/$1.conf"
+temp="/tmp/teeworlds.$RANDOM.conf"
+
+# Copy existing config into temp file
+cat $config > $temp
+
+# Append password
+echo "sv_rcon_password $adminpass" >> $temp
+echo "sv_rcon_mod_password $modpass" >> $temp
+echo "ec_password $consolepass" >> $temp
+
+~/server/openfng_srv -f $temp
